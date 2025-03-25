4c009a2f451df22fcc4f3182a371407ad3caad84
zhijian <v-zhijianxu@microsoft.com>
2025-03-25 09:23:26 +0000

    llm prediction pipeline usable

--- scripts/LLM/startup_vllm.sh
+++ scripts/LLM/startup_vllm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Controller with vLLM workers for maximum throughput
+export HF_HOME=/data/LocalLargeFiles/unsloth
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
 pkill -f fastchat
@@ -18,13 +19,14 @@ python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8021 \
     --api-keys EMPTY &
 
 # Dynamically get the number of GPUs and launch a vLLM worker for each
-NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+# NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+NUM_GPUS=1
 for GPU_ID in $(seq 0 $((NUM_GPUS - 1)))
 do
     echo "Launching worker on GPU $GPU_ID"
     CUDA_VISIBLE_DEVICES=$GPU_ID python3 -m fastchat.serve.vllm_worker \
-        --model-path /data/LocalLargeFiles/unsloth/models--meta-llama--Meta-Llama-3.1-8B-Instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659 \
-        --model-name local-model\
+        --model-path Qwen/Qwen2.5-14B-Instruct \
+        --model-name Qwen/Qwen2.5-14B-Instruct \
         --controller http://localhost:8020 \
         --worker-address http://localhost:$((24002 + GPU_ID)) \
         --host 0.0.0.0 \
@@ -32,7 +34,9 @@ do
         --tensor-parallel-size 1 \
         --gpu-memory-utilization 0.95 \
         --max-num-seqs 128 \
-        --max-model-len 8192 \
+        --max-model-len 20480 \
         --disable-log-requests &
 
 done
+
+wait
