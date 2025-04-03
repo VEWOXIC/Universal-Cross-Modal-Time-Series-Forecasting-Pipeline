511aa2e4aed98ad400ad825655418fabd86128d2
zhijian <v-zhijianxu@microsoft.com>
2025-04-03 10:00:45 +0000

    amlt pipeline

---
+++ scripts/LLM/llm_test_solar_day.sh
@@ -0,0 +1,9 @@
+python -u llm_run.py \
+    --model 'deepseek-ai/DeepSeek-R1-Distill-Qwen-14B' \
+    --model_config './model_configs/LLM/DeepSeek-R1-Distill-Qwen-14B.yaml' \
+    --data solar \
+    --data_config './data_configs/fullsolar_hetero.yaml' \
+    --ahead day \
+    --checkpoints /data/Blob_WestJP/v-zhijianxu/Reasoning_baselines/checkpoints \
+    --scale False
+    
