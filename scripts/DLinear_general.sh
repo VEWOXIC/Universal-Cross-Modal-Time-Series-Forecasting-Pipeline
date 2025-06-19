743129bf927b50f4e0c939c5a0cc6d9a1d7671c2
caiwx <caiwx22@mails.tsinghua.edu.cn>
2025-06-19 17:59:20 +0800

    add other dataset config (e.g. ETT)

--- scripts/DLinear_general.sh
+++ scripts/DLinear_general.sh
@@ -1,8 +1,8 @@
 python -u run.py \
     --model 'DLinear' \
     --model_config 'model_configs/general/DLinear.yaml' \
-    --data traffic \
-    --data_config './data_configs/fulltraffic.yaml' \
+    --data ETT \
+    --data_config './data_configs/fullETT.yaml' \
     --input_len 288 \
     --output_len 96 \
     --batch_size 128 \
--- scripts/PatchTST_general.sh
+++ scripts/PatchTST_general.sh
@@ -1,11 +1,11 @@
 python -u run.py \
     --model 'PatchTST' \
     --model_config 'model_configs/general/PatchTST.yaml' \
-    --data weather \
-    --data_config './data_configs/weather.yaml' \
+    --data traffic \
+    --data_config './data_configs/fulltraffic.yaml' \
     --input_len 288 \
     --output_len 96 \
-    --num_workers 16 \
     --downsample 6 \
-    --batch_size 1024 #| tee ./logs/traffic/PatchTST_day.log
+    --batch_size 128 \
+    --learning_rate 0.001 #| tee ./logs/traffic/PatchTST_day.log
     
