9037fe5592a40237094149df6717d5a7c489e2cb
caiwx <caiwx22@mails.tsinghua.edu.cn>
2025-06-25 20:14:37 +0800

    add dataset: fullCAISO

--- scripts/TGTSF_general.sh
+++ scripts/TGTSF_general.sh
@@ -6,15 +6,15 @@ do
 # python -u run_lightning.py \
 python -u run.py \
     --model 'TGTSF' \
-    --model_config 'model_configs/general/TGTSF-solar.yaml' \
-    --data solar \
-    --data_config './data_configs/fullsolar_hetero_emb.yaml' \
+    --model_config 'model_configs/general/TGTSF-CAISO.yaml' \
+    --data CAISO \
+    --data_config './data_configs/fullCAISO_hetero_emb.yaml' \
     --input_len 288 \
     --output_len 96 \
     --batch_size 256 \
     --noise $noise \
     --patience 10 \
-    --learning_rate 0.001 \
+    --learning_rate 0.0001 \
     # --devices 0,2,3 | tee ./logs/weather/TGTSF_96_$noise.log
     # --num_workers 16 \
     # --use_multi_gpu \
