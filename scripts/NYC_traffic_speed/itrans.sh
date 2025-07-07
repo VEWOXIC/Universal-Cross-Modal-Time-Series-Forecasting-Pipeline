88b899789513da659e75f90218b44406d7b8e1bd
Xu Zhijian <1155170452@link.cuhk.edu.hk>
2025-07-07 19:14:31 +0800

    update: lightning for trans EXP

--- scripts/NYC_traffic_speed/itrans.sh
+++ scripts/NYC_traffic_speed/itrans.sh
@@ -1,12 +1,12 @@
 for output_len in 24 168 336 720
 do
-python -u run.py \
+python -u run_lightning.py \
     --model 'iTransformer' \
     --model_config 'model_configs/general/iTransformer.yaml' \
     --data NYC_traffic_speed \
     --data_config './data_configs/NYC_traffic_speed/fullNYCTS_H.yaml' \
     --input_len 360 \
     --output_len $output_len \
-    --batch_size 512 | tee -a ./logs/Trans.log
+    --batch_size 128 | tee -a ./logs/Trans.log
 
 done
