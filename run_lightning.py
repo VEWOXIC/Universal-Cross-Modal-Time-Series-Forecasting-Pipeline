3040a88ada8cb14ddbf861b2826789ecf62bf05a
Xu Zhijian <1155170452@link.cuhk.edu.hk>
2025-07-14 19:55:55 +0800

    update: LLM samples v1

--- run_lightning.py
+++ run_lightning.py
@@ -44,7 +44,7 @@ parser.add_argument('--lradj', type=str, default='type3', help='Learning rate ad
 parser.add_argument('--use_gpu', type=bool, default=True, help='Whether to use GPU for training (if available)')
 parser.add_argument('--gpu', type=int, default=0, help='GPU device ID to use when not using multi-GPU')
 parser.add_argument('--use_multi_gpu', action='store_true', help='Use multiple GPUs for distributed training with DDP', default=True)
-parser.add_argument('--devices', type=str, default='2', help='Comma-separated list of GPU device IDs to use for multi-GPU training')
+parser.add_argument('--devices', type=str, default='0,1,4', help='Comma-separated list of GPU device IDs to use for multi-GPU training')
 
 # PyTorch Lightning specific
 parser.add_argument('--precision', type=str, default='32', help='Numerical precision for training: "32" (float32), "16" (float16/half), or "bf16" (bfloat16)')
