8f8ef7bdf13e78552958ca83928877a1de5d4413
Xu Zhijian <1155170452@link.cuhk.edu.hk>
2025-09-12 16:01:58 +0800

    update: Multimodal Bear on 1M

--- scripts/LLM/Qwen2.5-14B-Instruct-1m/Unimodal_Bear_room_hour.sh
+++ scripts/LLM/Qwen2.5-14B-Instruct-1m/Unimodal_Bear_room_hour.sh
@@ -1,6 +1,6 @@
 python -u run_llm.py \
     --model 'qwen2.5-14b-instruct-1m' \
-    --model_config './model_configs/LLM/MultiModal/Qwen2.5-14B-Instruct-1m.yaml' \
+    --model_config './model_configs/LLM/UniModal/Qwen2.5-14B-Instruct-1m.yaml' \
     --data Bear_room \
     --data_config './data_configs/Bear_room/fullBear_hetero_LLM.yaml' \
     --input_len 288 \
